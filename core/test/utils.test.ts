add7ae012f5c3903296fbf0ef06e3631e379c2cc
jaywcjlove <398188662@qq.com>
2023-06-07 16:07:40 +0800

    test: add test case.

--- core/test/utils.test.ts
+++ core/test/utils.test.ts
@@ -1,5 +1,14 @@
-import { merge } from "../src";
+import path from 'path';
+import { merge, autoConf, getConfigPath } from "../src";
 
 test('merge test case', () => {
   expect(merge({ a: 1}, { b: 2, a: 3})).toEqual({ a: 3, b: 2});
+});
+
+test('getConfigPath test case', () => {
+  const data = autoConf<{ projectName: string; }>(undefined, {
+    cwd: path.resolve(__dirname, '../../config-example/ext-json'),
+  });
+  expect(data?.projectName).toBe('ext-json');
+  expect(getConfigPath().endsWith('.autoconfrc.json')).toBeTruthy();
 });
